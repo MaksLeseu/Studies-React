a1b188f3fe9ab5c455378ab04567bd14131f8747
Maxim Lesev <lesov0iblr@gmail.com>
2023-02-16 13:41:54 +0100

    04 - counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Button} from "./04/04";
 
 function App() {
-    const subst1 = (name: string) => {
-        alert(name);
+    let [counter, setCounter] = useState(0);
+    const subst1 = (number: number) => {
+        setCounter(++counter);
     }
-    const subst2 = (name: string) => {
-        alert(name);
+
+    const resetSubst1 = (number: number) => {
+        setCounter(counter = 0);
     }
 
   return (
       <div className={'container'}>
-          <Button name={'First button'} callback={() => subst1('My name is Maks')} />
-          <Button name={'Second button'} callback={() => subst2('Ma name is Fil')} />
+          <h3 className={'counter'}>{counter}</h3>
+          <Button name={'BUTTON'} callback={() => subst1(counter)} />
+          <Button name={'reset'} callback={() => resetSubst1(counter)} />
       </div>
   );
 }
